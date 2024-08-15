2aadb3e8a886fa11931891cf8b8fd6eab0988587
Sumi <sumiran.vasala@aplaceformom.com>
2024-08-15 10:36:16 -0500

    header

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,10 @@
-import React from "react"
-import AboutCard from "../about/AboutCard"
-import Hblog from "./Hblog"
-import HAbout from "./HAbout"
-import Hero from "./hero/Hero"
-import Hprice from "./Hprice"
-import Testimonal from "./testimonal/Testimonal"
+import React from "react";
+import AboutCard from "../about/AboutCard";
+import Hblog from "./Hblog";
+import HAbout from "./HAbout";
+import Hero from "./hero/Hero";
+import Hprice from "./Hprice";
+import Testimonal from "./testimonal/Testimonal";
 
 const Home = () => {
   return (
@@ -16,7 +16,7 @@ const Home = () => {
       <Hblog />
       <Hprice />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
